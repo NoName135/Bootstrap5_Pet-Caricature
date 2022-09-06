e62575a45e07300076f0340909fcde19ea078e79
Karuma Lin <qscwdvefb3@gmail.com>
2022-09-06 22:17:14 +0800

    update

--- all.js
+++ all.js
@@ -1,18 +1,18 @@
-document.querySelector('.custom-picture-button').style.display = 'none';
+// document.querySelector('.custom-picture-button').style.display = 'none';
 
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 885 ||
-    document.documentElement.scrollTop > 885
-  ) {
-    if (window.matchMedia('(min-width: 992px)').matches){
-      document.querySelector('.custom-picture-button').style.display = 'block';
-    }
-  } else {
-    document.querySelector('.custom-picture-button').style.display = 'none';
-  }
-}
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 885 ||
+//     document.documentElement.scrollTop > 885
+//   ) {
+//     if (window.matchMedia('(min-width: 992px)').matches){
+//       document.querySelector('.custom-picture-button').style.display = 'block';
+//     }
+//   } else {
+//     document.querySelector('.custom-picture-button').style.display = 'none';
+//   }
+// }
